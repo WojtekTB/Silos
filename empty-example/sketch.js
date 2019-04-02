7fe1cae7ea51551420e6c9bf49f55d7e3912c24b
WojtekTB <34536619+WojtekTB@users.noreply.github.com>
2019-04-02 14:36:04 -0700

    Added p5js library

---
+++ empty-example/sketch.js
@@ -0,0 +1,20 @@
+var screenX = window.innerWidth;
+var screenY = window.innerHeight - 20;
+
+
+function setup() {
+  createCanvas(screenX, screenY);
+}
+
+function draw(){
+  // put drawing code here
+  background(`rgb(255, 255, 0)`);
+  rect(10, 10, 30, 30);
+}
+
+function keyPressed(){
+  if(keyCode = ' ')
+  {
+    console.log(" ");
+  }
+}
