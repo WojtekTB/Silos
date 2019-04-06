37bbce84f0d042e84c898e7646052fc345cc2de5
WojtekTB <34536619+WojtekTB@users.noreply.github.com>
2019-04-06 11:42:51 -0700

    added capabilities for layers of collision points
    added capabilities for layers of collision points through arrays, and now displays all the points as dots on the screen for clarity

--- empty-example/javascript/PlayerClass.js
+++ empty-example/javascript/PlayerClass.js
@@ -1,5 +1,5 @@
 class Player{
-  constructor(startingX, startingY, map){//initialize object
+  constructor(startingX, startingY, map, images){//initialize object
     //-----position-----
     this.x = startingX;
     this.y = startingY;
@@ -11,8 +11,10 @@ class Player{
     this.overallVelocity = 0;//total force acting on sprite
     this.gravity = 0.5;//downward pull
     //-----sprite-----
-    this.spriteHeight = 100;
-    this.spriteWidth = 50;
+    this.spriteHeight = 95;
+    this.spriteWidth = 157;
+
+    this.image = images;
     //-----player-statuses-----
     this.crouched = 0;//[0 = standing; 1 = crouching] => start off standing
     this.maxJumps = 2;//constant max number of jumps to reset to
@@ -24,17 +26,66 @@ class Player{
     this.mapTiles = map.mapTiles;
     console.log(this.mapScale, this.mapTiles);
     //-----player collision values-----
-    this.collisionPoints = {
+    this.collisionPointBottom = {
       cpx: this.x,
       cpy: this.y + this.spriteHeight/2
     }
-    this.previousCollisionPoints = {
-      pcpx: this.collisionPoints.cpx,
-      pcpy: this.collisionPoints.cpy
+    this.collisionPointBottomRight = {
+      cpx: this.x + this.spriteWidth/2,
+      cpy: this.y + this.spriteHeight/2
     }
+    this.collisionPointBottomLeft = {
+      cpx: this.x - this.spriteWidth/2,
+      cpy: this.y + this.spriteHeight/2
+    }
+
+    this.collisionPointsOnEachSide = 4;
+
+    this.bottomCollisionArrayX = [];
+    for(let i = 0; i < this.collisionPointsOnEachSide; i++){
+      this.bottomCollisionArrayX.push((this.x - (this.spriteWidth/2)) + i * (this.spriteWidth/this.collisionPointsOnEachSide));
+    }
+
+    this.rightCollisionArrayY = [];
+    for(let i = 0; i < this.collisionPointsOnEachSide; i++){
+      this.rightCollisionArrayY.push((this.y - (this.spriteHeight/2)) + i * (this.spriteHeight/this.collisionPointsOnEachSide));
+    }
+
+    this.leftCollisionArrayY = [];
+    for(let i = 0; i < this.collisionPointsOnEachSide; i++){
+      this.leftCollisionArrayY.push((this.y - (this.spriteHeight/2)) + i * (this.spriteHeight/this.collisionPointsOnEachSide));
+    }
+
+
+
+  // this.previousCollisionPoints = {
+  //     pcpx: this.collisionPoints.cpx,
+  //     pcpy: this.collisionPoints.cpy
+  //   }
+
     this.characterTiles = {
-      tileX: Math.floor(this.collisionPoints.cpx / this.mapScale),
-      tileY: Math.floor(this.collisionPoints.cpy / this.mapScale)
+      tileXM: Math.floor(this.collisionPointBottom.cpx / this.mapScale),//Mid
+      tileYM: Math.floor(this.collisionPointBottom.cpy / this.mapScale),
+        tileXR: Math.floor(this.collisionPointBottom.cpx / this.mapScale),//Right
+        tileYR: Math.floor(this.collisionPointBottom.cpy / this.mapScale) - 5,
+          tileXL: Math.floor(this.collisionPointBottom.cpx / this.mapScale),//left
+          tileYL: Math.floor(this.collisionPointBottom.cpy / this.mapScale)- 5
+    }
+    // this.previousCharacterTiles = {
+    //   tileX: this.characterTiles.tileX,
+    //   tileY: this.characterTiles.tileX
+    // }
+  }
+
+  collisionPointsUpdate(){
+    for(let i = 0; i < this.collisionPointsOnEachSide; i++){
+      this.bottomCollisionArrayX[i] = this.x - (this.spriteWidth/2) + i * (this.spriteWidth/this.collisionPointsOnEachSide);
+    }
+    for(let i = 0; i < this.collisionPointsOnEachSide; i++){
+      this.rightCollisionArrayY[i] = (this.y - (this.spriteHeight/2)) + i * (this.spriteHeight/this.collisionPointsOnEachSide) - 2;
+    }
+    for(let i = 0; i < this.collisionPointsOnEachSide; i++){
+      this.leftCollisionArrayY[i] = (this.y - (this.spriteHeight/2)) + i * (this.spriteHeight/this.collisionPointsOnEachSide) - 2;
     }
   }
 
@@ -79,55 +130,204 @@ class Player{
 
   show(){//render
     fill(255);
-    rect(this.x - (this.spriteWidth / 2), this.y - (this.spriteHeight / 2), this.spriteWidth, this.spriteHeight);
+    // rect(this.x - (this.spriteWidth / 2), this.y - (this.spriteHeight / 2), this.spriteWidth, this.spriteHeight);
+    image(this.image, this.x - (this.spriteWidth / 2), this.y - (this.spriteHeight / 2), this.spriteWidth, this.spriteHeight);
   }
 
   collisionDetection(){
-    this.previousCollisionPoints = {
-      pcpx: this.collisionPoints.cpx,
-      pcpy: this.collisionPoints.cpy
-    }
-    this.collisionPoints = {
+    // let movedUp = false;
+    // let movedDown = false;
+    // let movedLeft = false;
+    // let movedRight = false;
+
+    // this.previousCollisionPoints = {
+    //   pcpx: this.collisionPoints.cpx,
+    //   pcpy: this.collisionPoints.cpy
+    // }
+
+    let bumpMid = false;
+    let bumpRight = false;
+    let bumpLeft = false;
+    this.collisionPointBottom = {
       cpx: this.x,
       cpy: this.y + this.spriteHeight/2
     }
-    this.characterTiles = {
-      tileX: Math.floor(this.collisionPoints.cpx / this.mapScale),
-      tileY: Math.floor(this.collisionPoints.cpy / this.mapScale)
+    this.collisionPointBottomRight = {
+      cpx: this.x + this.spriteWidth/2,
+      cpy: this.y + this.spriteHeight/2-5
     }
-    console.log("Character X tile: " + this.characterTiles.tileX + "  Character Y tile: " +  this.characterTiles.tileY);
-    console.log("Collision point X tile: " + this.collisionPoints.cpx + "  Collision point Y tile: " +  this.collisionPoints.cpy);
+    this.collisionPointBottomLeft = {
+      cpx: this.x - this.spriteWidth/2,
+      cpy: this.y + this.spriteHeight/2 - 5
+    }
+    // this.previousCharacterTiles = {
+    //   tileX: this.characterTiles.tileX,
+    //   tileY: this.characterTiles.tileY
+    // }
 
-    console.log(this.previousCollisionPoints.previousX, this.previousCollisionPoints.previousY);
-    if(this.mapTiles[this.characterTiles.tileY][this.characterTiles.tileX] === 1){
-      console.log(1);
-      this.x = this.previousX;
+    this.characterTiles = {
+      tileXM: Math.floor(this.collisionPointBottom.cpx / this.mapScale),//Mid
+      tileYM: Math.floor(this.collisionPointBottom.cpy / this.mapScale),
+        tileXR: Math.floor(this.collisionPointBottomRight.cpx / this.mapScale),//Right
+        tileYR: Math.floor(this.collisionPointBottomRight.cpy / this.mapScale) ,
+          tileXL: Math.floor(this.collisionPointBottomLeft.cpx / this.mapScale),//left
+          tileYL: Math.floor(this.collisionPointBottomLeft.cpy / this.mapScale)
+    }
+
+    console.log([this.mapTiles[this.characterTiles.tileYL][this.characterTiles.tileXL], this.mapTiles[this.characterTiles.tileYM][this.characterTiles.tileXM], this.mapTiles[this.characterTiles.tileYR][this.characterTiles.tileXR]]);
+    if(this.mapTiles[this.characterTiles.tileYL][this.characterTiles.tileXL] === 1){//left bump
+      this.x += 5;
+
+    }
+    if(this.mapTiles[this.characterTiles.tileYM][this.characterTiles.tileXM] === 1){//mid bump
       this.y = this.previousY;
+      this.jumpNumber = this.maxJumps;
       this.overallVelocity = 0;
     }
-    else{
-      console.log(0);
+    if(this.mapTiles[this.characterTiles.tileYR][this.characterTiles.tileXR] === 1){//right bump
+      this.x -= 5;
     }
 
-
-
-
-
-
-    // let column = this.mapTiles[this.characterTiles.tileY-1];
-    // // console.log(column);
-    // // console.log("this number is " + column[Math.floor(this.collisionPoints.cpx / this.mapScale)]);
-    // if (column[this.characterTiles.tileX] === 1)
+    // if(this.x > this.previousX){
+    //   this.movedRight = true;
+    // }
+    // else if(this.x < this.previousX){
+    //   this.movedLeft = true;
+    // }
+    // else if(this.y > this.previousY){
+    //   this.movedDown = true;
+    // }
+    // else if(this.y > this.previousY){
+    //   this.movedUp = true;
+    // }
+    //
+    //   console.log("-----v--------v------");
+    // if(this.mapTiles[this.characterTiles.tileY][this.characterTiles.tileX] === 1)//up and down
     // {
-    //   // this.y = this.previousY;
-    //   // this.overallVelocity = 0;
-    //   // console.log("inside the block");
+    //   if(this.movedDown)
+    //   {
+    //     console.log("Down");
+    //     this.y = this.previousY;
+    //     this.overallVelocity = 0;
+    //     this.jumpNumber = this.maxJumps;
+    //   }
+    //   else if(this.movedUp)
+    //   {
+    //     console.log("Up");
+    //     this.y = this.previousY;
+    //     this.overallVelocity = 0;
+    //   }
+    //   if(this.movedLeft)
+    //   {
+    //     console.log("Left");
+    //     this.x += 100;
+    //   }
+    //   else if(this.movedRight)
+    //   {
+    //     console.log("Right");
+    //     this.x -= 100;
+    //   }
     //   this.inTheAir = 0;
-    //   this.y = (this.mapTiles[this.characterTiles.tileY][this.characterTiles.tileX])*this.mapScale*2;
     // }
     // else{
+    //   console.log("IN AIR");
     //   this.inTheAir = 1;
     // }
+    // console.log(this.characterTiles.tileY, this.characterTiles.tileX);
+
+    // if(this.previousCharacterTiles.tileX != Math.floor(this.collisionPoints.cpx / this.mapScale))//if moved on x
+    // {
+    //   // console.log("moved on X");
+    //   // console.log("was: " +this.previousCharacterTiles.tileX + "  now is:" + Math.floor(this.collisionPoints.cpx / this.mapScale));
+    //
+    //   let lastX = this.previousCharacterTiles.tileX;
+    //   let newX = Math.floor(this.collisionPoints.cpx / this.mapScale);
+    //   //-----if moved to the right-----
+    //   if(lastX < newX){
+    //     console.log("Moved Right");
+    //     movedRight = true;
+    //   }
+    //   //-----if moved to the left-----
+    //   if(lastX > newX){
+    //     console.log("Moved Left");
+    //     movedRight = true;
+    //   }
+    // }
+    // if(this.previousCharacterTiles.tileY != Math.floor(this.collisionPoints.cpy / this.mapScale))//if moved on y
+    // {
+    //   // console.log("moved on Y");
+    //   // console.log("was: " + this.previousCharacterTiles.tileX + "  now is:" + Math.floor(this.collisionPoints.cpy / this.mapScale));
+    //
+    //   let lastY = this.previousCharacterTiles.tileY;
+    //   let newY = Math.floor(this.collisionPoints.cpy / this.mapScale);
+    //   //-----if moved down-----
+    //   if(lastY < newY){
+    //     console.log("Moved Down");
+    //     movedDown = true;
+    //   }
+    //   //-----if moved UP-----
+    //   if(lastY > newY){
+    //     console.log("Moved UP");
+    //     movedUp = true;
+    //   }
+    // }
+    // this.previousCharacterTiles = {
+    //   tileX: this.characterTiles.tileX,
+    //   tileY: this.characterTiles.tileY
+    // }
+    // this.characterTiles = {
+    //   tileX: Math.floor(this.collisionPoints.cpx / this.mapScale),
+    //   tileY: Math.floor(this.collisionPoints.cpy / this.mapScale)
+    // }
+    // // console.log("Character X tile: " + this.characterTiles.tileX + "  Character Y tile: " +  this.characterTiles.tileY);
+    // // console.log("Collision point X tile: " + this.collisionPoints.cpx + "  Collision point Y tile: " +  this.collisionPoints.cpy);
+    // //
+    // // console.log(this.previousCollisionPoints.previousX, this.previousCollisionPoints.previousY);
+    // if(this.mapTiles[this.characterTiles.tileY][this.characterTiles.tileX] === 1){//if landed on tile
+    //   // console.log(1);
+    //   if(movedUp){
+    //
+    //     this.y = this.previousY - 1;
+    //     this.overallVelocity = 0;
+    //   }
+    //   if(movedDown){
+    //     this.y = this.previousY - 1;
+    //     this.overallVelocity = 0;
+    //   }
+    //   if(movedLeft){
+    //     this.x = this.previousX;
+    //     this.overallVelocity = 0;
+    //   }
+    //   this.jumpNumber = this.maxJumps;
+    //   }
+    //   if(movedRight){
+    //     this.y = this.previousY;
+    //     this.overallVelocity = 0;
+    //   }
+    //   else{
+    //   movedLeft = false;
+    //   movedRight = false;
+    //   movedUp = false;
+    //   movedDown = false;
+    //     // console.log(0);
+    //   }
+    //
+    //
+    //
+    // // let column = this.mapTiles[this.characterTiles.tileY-1];
+    // // // console.log(column);
+    // // // console.log("this number is " + column[Math.floor(this.collisionPoints.cpx / this.mapScale)]);
+    // // if (column[this.characterTiles.tileX] === 1)
+    // // {
+    // //   // this.y = this.previousY;
+    // //   // this.overallVelocity = 0;
+    // //   // console.log("inside the block");
+    // //   this.inTheAir = 0;
+    // //   this.y = (this.mapTiles[this.characterTiles.tileY][this.characterTiles.tileX])*this.mapScale*2;
+    // // }
+    // // else{
+    // //   this.inTheAir = 1;
+    // // }
   }
 
   update(){//update values
@@ -141,6 +341,7 @@ class Player{
       this.overallVelocity = 0;
       this.jumpNumber = this.maxJumps;
     }
+    this.collisionPointsUpdate();
     // console.log(this.overallVelocity);
   }
 }
--- empty-example/javascript/sketch.js
+++ empty-example/javascript/sketch.js
@@ -6,29 +6,31 @@ var mapScale = screenY;
 
 var images = {
   brick: null,
-  stone: null
+  stone: null,
+  player: null
 };
 
 var mapTiles = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
 function preload(){
   images.brick = loadImage('assets/brick.png');
+  images.player = loadImage('assets/yeet.png');
 }
 
 function setup() {
   createCanvas(screenX, screenY);
   map1 = new Map(mapTiles, images, mapScale);
-  player = new Player(screenX/2, screenY/2, map1);
+  player = new Player(screenX/2, screenY/2, map1, images.player);
 }
 
 function draw(){
@@ -38,7 +40,21 @@ function draw(){
   player.update();
   player.show();
   fill(`rgb(255, 0, 0)`);
-  rect(player.collisionPoints.cpx, player.collisionPoints.cpy, 3, 3);
+  rect(player.collisionPointBottom.cpx, player.collisionPointBottom.cpy, 3, 3);//mid
+  rect(player.collisionPointBottomRight.cpx, player.collisionPointBottomRight.cpy, 3, 3);//right
+  rect(player.collisionPointBottomLeft.cpx, player.collisionPointBottomLeft.cpy, 3, 3);//left
+
+  for(i = 0; i < player.bottomCollisionArrayX.length; i++){
+    rect(player.bottomCollisionArrayX[i], player.collisionPointBottom.cpy, 3, 3);
+  }
+  fill(`rgb(0, 0, 255)`);
+  for(i = 0; i < player.rightCollisionArrayY.length; i++){
+    rect(player.collisionPointBottomLeft.cpx + player.spriteWidth, player.rightCollisionArrayY[i], 3, 3);
+  }
+  fill(`rgb(0, 255, 0)`);
+  for(i = 0; i < player.leftCollisionArrayY.length; i++){
+    rect(player.collisionPointBottomLeft.cpx, player.leftCollisionArrayY[i], 3, 3);
+  }
 
 }
 
